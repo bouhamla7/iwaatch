a11a78b5609162f775196ba9a211503b85ac8fa8
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2024-07-24 02:08:33 +0530

    chore: add discord invitation on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,27 @@ import styles from "@/styles/Home.module.scss";
 import HomeHero from "@/components/HomeHero";
 import HomeListAll from "@/components/HomeListAll";
 
+// discord Invitation
+import { useEffect } from "react";
+import { toast } from "sonner";
+import { FaDiscord } from "react-icons/fa";
+// discord Invitation
+
 export default function Home() {
+  // discord Invitation
+  useEffect(() => {
+    toast.info(
+      <a
+        target="_blank"
+        href="https://discord.gg/6xJmJja8fV"
+        className="discordInvitation"
+      >
+        <FaDiscord />
+        Be part of our Rive community on Discord
+      </a>,
+    );
+  });
+  // discord Invitation
   return (
     <div className={styles.Home}>
       <HomeHero />
