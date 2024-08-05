323770c07316ae0d724476877af8ea84172cb71f
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2024-08-05 15:51:51 +0530

    fix: headers for website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -86,13 +86,24 @@ export default function App({ Component, pageProps }: any) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Rive" />
-        <link rel="icon" href="/images/logo512.png" />
-        <link rel="apple-touch-icon" href="/images/logo512.png" />
-        {/* <link rel="mask-icon" href="/images/logo512.svg" color="#f4f7fe" /> */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icons/icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/images/logo512.svg"
+          color="#f4f7fe"
+        />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <meta name="mobile-web-app-capable" content="yes" />
         {/* <meta name="msapplication-TileColor" content="#f4f7fe" /> */}
         <meta name="msapplication-tap-highlight" content="no" />
         <link rel="shortcut icon" href="/images/logo512.png" />
+        <link rel="apple-touch-startup-image" href="/images/logo512.svg" />
       </Head>
       <Layout>
         <Toaster
