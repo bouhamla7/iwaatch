fcf1ec3cf91537bb390d2e9f277b3490460d475c
bouhamla7 <89969839+bouhamla7@users.noreply.github.com>
2024-08-20 00:40:03 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -85,7 +85,7 @@ export default function App({ Component, pageProps }: any) {
           name="twitter:image"
           content="https://rivestream.live/images/MetaImage.jpg"
         />
-
+<script defer src="https://cloud.umami.is/script.js" data-website-id="ed6f84c1-0813-4741-89c5-03108615fecf"></script>
       </Head>
       <Layout>
         <Toaster
