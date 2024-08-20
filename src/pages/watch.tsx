117542e0a6ef4d9b62aae485535263a0e4d946aa
bouhamla7 <89969839+bouhamla7@users.noreply.github.com>
2024-08-20 14:46:51 +0100

    Update watch.tsx

--- src/pages/watch.tsx
+++ src/pages/watch.tsx
@@ -148,6 +148,41 @@ const Watch = () => {
     // Remove event listener when component unmounts
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
+          toast.info(
+        <div>
+          Cloud: use AD-Blocker services for AD-free experience, like AD-Blocker
+          extension or{" "}
+          <a target="_blank" href="https://brave.com/">
+            Brave Browser{" "}
+          </a>
+        </div>,
+      );
+
+      toast.info(
+        <div>
+          Cloud: use video downloader extensions like{" "}
+          <a target="_blank" href="https://fetchv.net/">
+            FetchV{" "}
+          </a>{" "}
+          or{" "}
+          <a target="_blank" href="https://www.hlsloader.com/">
+            Stream Recorder{" "}
+          </a>{" "}
+          for PC and{" "}
+          <a
+            target="_blank"
+            href="https://play.google.com/store/apps/details?id=videoplayer.videodownloader.downloader"
+          >
+            AVDP{" "}
+          </a>{" "}
+          for Android, to download movies/tv shows. Refer{" "}
+          <a
+            target="_blank"
+            href="https://www.reddit.com/r/DataHoarder/comments/qgne3i/how_to_download_videos_from_vidsrcme/"
+          >
+            The Source{" "}
+          </a>
+        </div>,
     };
   }, [params, id, season, episode]);
 
