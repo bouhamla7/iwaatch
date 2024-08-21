f75ea706db49deea688a3f0ef5f599276f7c74a9
bouhamla7 <89969839+bouhamla7@users.noreply.github.com>
2024-08-21 01:41:08 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,15 +13,13 @@ import "@/styles/nprogress.scss";
 import "react-loading-skeleton/dist/skeleton.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import AdSense from "@/components/AdSense";
-import { AdblockDetector } from 'adblock-detector';
-import { Ads } from "@/components/Ads"
+
 
 export default function App({ Component, pageProps }: any) {
   const [isLoading, setIsLoading] = useState(false);
   NProgress.configure({ showSpinner: false });
   const GTag: any = process.env.NEXT_PUBLIC_GT_MEASUREMENT_ID;
-  const adbDetector = new AdblockDetector() // call 
-  const userHasAdblock = adbDetector.detect() // detect adblock it return ture or false
+ 
 
   return (
     <>
@@ -106,7 +104,6 @@ export default function App({ Component, pageProps }: any) {
           position="top-center"
         />
         <Tooltip id="tooltip" className="react-tooltip" />
-         {userHasAdblock ? <Ads /> :}
 
         <Component {...pageProps} />
       </Layout>
