3abfb1768219d2d6e829a2e5c25b1c1dbf479a2b
bouhamla7 <89969839+bouhamla7@users.noreply.github.com>
2024-08-21 01:25:38 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ import styles from "@/styles/Home.module.scss";
 import HomeHero from "@/components/HomeHero";
 import HomeListAll from "@/components/HomeListAll";
 import AdSense from "@/components/AdSense";
-
+import { AdblockDetector } from 'adblock-detector';
+import { Ads } from "@/components/Ads"
 // discord Invitation
 import { useEffect } from "react";
 import { toast } from "sonner";
@@ -14,13 +15,15 @@ import useDeviceSize from "@/Utils/useDeviceSize";
 
 export default function Home() {
   // discord Invitation
-
+const adbDetector = new AdblockDetector() // call 
+const userHasAdblock = adbDetector.detect() // detect adblock it return ture or false
   // discord Invitation
   return (
     <div className={styles.Home}>
       <HomeHero />
       <HomeListAll />
       <AdSense pId="ca-pub-9098691343505810"/>
+      {userHasAdblock ? <Ads /> : null}
 
     </div>
   );
