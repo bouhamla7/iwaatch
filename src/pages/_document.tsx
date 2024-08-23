618656a15d988c1b05196fc3dbc11fca67811cc4
bouhamla7 <89969839+bouhamla7@users.noreply.github.com>
2024-08-23 23:46:14 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAdSense } from "next-google-adsense";
 
 export default function Document() {
   return (
     <Html lang="en">
-
+     <GoogleAdSense />
       <Head />
       <body>
         <Main />
