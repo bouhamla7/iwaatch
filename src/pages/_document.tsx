6d6a3c66cdcb4502ce34dcf80e8a939c041e9eed
bouhamla7 <89969839+bouhamla7@users.noreply.github.com>
2024-08-23 21:53:56 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import AdSense from "@/components/AdSense";
 
 export default function Document() {
   return (
     <Html lang="en">
-    <AdSense pId="ca-pub-9098691343505810"/>
 
       <Head />
       <body>
