17d48e9c8d6ed0a32dc23a27c24538120e052b24
bouhamla7 <89969839+bouhamla7@users.noreply.github.com>
2024-08-24 15:20:50 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-              <Script
-    async
-    src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9098691343505810`}
-    strategy="lazyOnload"
-    crossOrigin="anonymous"
-   ></Script>
+
       <Head />
       <body>
         <Main />
